00b7764a30e220f9fe3af2047c559f5ceeb94cc5
CedricHuang <997630814@qq.com>
2022-02-24 21:44:53 -0800

    Add CreatePost function

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,13 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
+
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = { 
     Query: {
        async getPosts(){
            try{
-               const posts = await Post.find();
+               const posts = await Post.find().sort({ createdAt: -1 });
                return posts;
            } catch (err) {
                throw new Error(err);
@@ -22,5 +25,44 @@ module.exports = {
                 throw new Error(err);
             }
         }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+      
+            if (body.trim() === '') {
+              throw new Error('Post body must not be empty');
+            }
+      
+            const newPost = new Post({
+              body,
+              user: user.id,
+              username: user.username,
+              createdAt: new Date().toISOString()
+            });
+      
+            const post = await newPost.save();
+      
+            // context.pubsub.publish('NEW_POST', {
+            //   newPost: post
+            // });
+      
+            return post;
+          },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+    
+            try {
+                const post = await Post.findById(postId);
+                if (user.username === post.username) {
+                    await post.delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
+        },
     }
 }

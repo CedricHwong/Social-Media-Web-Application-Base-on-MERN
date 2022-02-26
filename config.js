3c4ce51810a8b9de040c53ad0c21b6e1ffa849ee
CedricHuang <997630814@qq.com>
2022-02-26 14:23:36 -0800

    add config.js for MongoDB Atlas

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB : "mongodb+srv://cedric:Qhuangguangwei123@cluster0.6b7o9.mongodb.net/SocialMediaApp?retryWrites=true&w=majority",
+    SECRET_KEY: 'some very secret key'
+}

7398f5ff8cc7cd2452279a23b1a0f78a7fa1236b
qinshou2017 <czy-ken@163.com>
2022-03-09 18:54:26 -0800

    add base page & menu bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css';
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Home, Login, Register } from './pages';
+import MenuBar from './components/MenuBar';
+
+const NotFound = () => <h1>Page Not Found</h1>;
 
 function App() {
   return (
-    <div>hello world</div>
+    <Router>
+      <MenuBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
